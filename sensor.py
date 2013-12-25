087babe72449d9c3f3e83add9fb9f5333fb726f3
Devasia Manuel <devasia1000@gmail.com>
2013-12-24 19:39:00 -0600

    added siganl strength detection

--- sensor.py
+++ sensor.py
@@ -10,16 +10,22 @@ ser = serial.Serial(port = sys.argv[1], baudrate = 4800);
 
 # keep reading from device
 while True:
+       
         line = str(ser.readline());
         
         # isolate tag that contains speed information
         if "GPRMC" in line:
-
+            
             #extract speed from line
             temp = line.split(",");
-            speed = float(temp[7]);
-            
-            # convert speed from knots to mph and write to STDOUT
-            sys.stdout.write(str(speed*1.15078)+"\n");
+
+            if not str(temp[7]):
+                # if NMEA is empty, print out 'No signal!'
+                sys.stdout.write("No signal!\n");
+
+            else:
+                speed = float(temp[7]);
+                # convert speed from knots to mph and write to STDOUT
+                sys.stdout.write(str(speed*1.15078)+"\n");
 
 ser.close();
