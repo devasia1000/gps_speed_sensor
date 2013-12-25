c46c07d4cabe40580b7c223ec8a69740562eaeff
Devasia Manuel <devasia1000@gmail.com>
2013-12-24 19:53:45 -0600

    sensor gui works

--- sensor.py
+++ sensor.py
@@ -1,16 +1,24 @@
 import serial, sys;
+from Tkinter import *
+import time
 
 # check for presence of device port argument
 if len(sys.argv) != 2:
     print "Usage: sudo python sensor.py <device port>";
     sys.exit(0);
 
+root=Tk()
+
 # initiate serial connection
 ser = serial.Serial(port = sys.argv[1], baudrate = 4800);
 
+# variable used to manage updated to GUI
+speed_str = StringVar()
+
 # keep reading from device
-while True:
-       
+def update_label():
+
+    while True:
         line = str(ser.readline());
         
         # isolate tag that contains speed information
@@ -22,10 +30,20 @@ while True:
             if not str(temp[7]):
                 # if NMEA is empty, print out 'No signal!'
                 sys.stdout.write("No signal!\n");
+                speed_str.set("No signal!\n");
 
             else:
                 speed = float(temp[7]);
                 # convert speed from knots to mph and write to STDOUT
                 sys.stdout.write(str(speed*1.15078)+"\n");
+                speed_str.set(str(speed*1.15078)+" mph");
 
-ser.close();
+            # update GUI
+            root.update();
+
+
+speed_label=Label(root, textvariable=speed_str, font=("Helvetica", 50))
+speed_label.pack()
+start_button=Button(root, text="start", command=update_label)
+start_button.pack()
+root.mainloop()
