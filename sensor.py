5ff2ece36b3c99eb2525449adbc5300fc5997fa4
Devasia Manuel <devasia1000@gmail.com>
2013-12-24 20:08:25 -0600

    wrote some comments

--- sensor.py
+++ sensor.py
@@ -2,7 +2,7 @@ import serial, sys;
 from Tkinter import *
 import time
 
-# check for presence of device port argument
+# check for presence of argument
 if len(sys.argv) != 2:
     print "Usage: sudo python sensor.py <device port>";
     sys.exit(0);
@@ -12,13 +12,14 @@ root=Tk()
 # initiate serial connection
 ser = serial.Serial(port = sys.argv[1], baudrate = 4800);
 
-# variable used to manage updated to GUI
+# synchronize updates to our GUI
 speed_str = StringVar()
 
 # keep reading from device
 def update_label():
 
     while True:
+
         line = str(ser.readline());
         
         # isolate tag that contains speed information
@@ -33,18 +34,23 @@ def update_label():
                 speed_str.set("No signal!\n");
 
             else:
+                # convert speed from knots to mph
                 speed = float(temp[7])*1.15078;
 
-                # convert speed from knots to mph and write to STDOUT
+                # format speed to 2 decimal places
                 sys.stdout.write(str(speed)+"\n");
                 speed_str.set("{0:.2f}".format(speed)+" mph");
 
             # update GUI
             root.update();
 
-
+# create label
 speed_label=Label(root, textvariable=speed_str, font=("Helvetica", 100))
 speed_label.pack()
+
+# create start button
 start_button=Button(root, text="start", command=update_label)
 start_button.pack()
+
+# start GUI thread
 root.mainloop()
